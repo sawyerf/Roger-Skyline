5cde182c0d0694477048e8d8dfd42bf479ad4ccd
Sawyerf <sawyer.flink@protonmail.ch>
2019-10-01 14:32:21 +0200

    Add: dos rules

--- iptables/iptables.sh
+++ iptables/iptables.sh
@@ -11,6 +11,10 @@ iptables -t filter -A INPUT -p udp -m udp --sport 53 -m conntrack --ctstate RELA
 iptables -t filter -A INPUT -p tcp --dport 1357 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -t filter -A OUTPUT -p tcp --sport 1357 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
+iptables -t filter -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -t filter -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 iptables -t filter -A OUTPUT -p tcp -m multiport --dports 80,443,8000 -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT  
 iptables -t filter -A INPUT -p tcp -m multiport --sports 80,443,8000 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
@@ -27,10 +31,18 @@ done
 
 iptables -t filter -A OUTPUT -o lo -j ACCEPT
 iptables -t filter -A INPUT -i lo -j ACCEPT
-iptables -I INPUT -p tcp --dport 80 -m connlimit --connlimit-above 40 --connlimit-mask 20 -j DROP
-iptables -I INPUT -p tcp --dport 443 -m connlimit --connlimit-above 40 --connlimit-mask 20 -j DROP
-iptables -I INPUT -p tcp --dport 1357 -m connlimit --connlimit-above 40 --connlimit-mask 20 -j DROP
+# iptables -I INPUT -p tcp --dport 80 -m connlimit --connlimit-above 40 --connlimit-mask 20 -j DROP
+# iptables -I INPUT -p tcp --dport 443 -m connlimit --connlimit-above 40 --connlimit-mask 20 -j DROP
+# iptables -I INPUT -p tcp --dport 1357 -m connlimit --connlimit-above 40 --connlimit-mask 20 -j DROP
 
 iptables -I INPUT -p tcp --dport 1357 -m state --state NEW -m recent --set
-iptables -I INPUT -p tcp --dport 1357 -m state --state NEW -m recent --update --seconds 10 --hitcount 2 -j DROP
-iptables -I INPUT -p tcp --dport 1357 -m connlimit --connlimit-above 2 -j DROP
+iptables -I INPUT -p tcp --dport 1357 -m state --state NEW -m recent --update --seconds 10 --hitcount 2 -j REJECT
+iptables -I INPUT -p tcp --dport 1357 -m connlimit --connlimit-above 2 -j REJECT
+
+PORTS="80 443"
+for PORT in $PORTS
+do
+	iptables -I INPUT -p tcp --dport $PORT -m state --state NEW -m recent --set
+	iptables -I INPUT -p tcp --dport $PORT -m state --state NEW -m recent --update --seconds 5 --hitcount 2 -j REJECT
+	iptables -I INPUT -p tcp --dport $PORT -m connlimit --connlimit-above 2 -j REJECT
+done
